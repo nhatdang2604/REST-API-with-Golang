0879d265896f912d7f6c17c1c8a90d806be64071
dangkl123 <nhatdang2604@gmail.com>
2022-07-22 22:21:15 +0700

    Add method to add an new album via POST

--- main.go
+++ main.go
@@ -15,23 +15,39 @@ type album struct {
 
 var (
 
-	ALBUMS = []album {
+	albums = []album {
 		{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-		{ID: "2", Title: "Jeru", Aritst: "Gerry Mulligan", Price: 17.99},
+		{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 		{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 	}
 
 	PATH = "localhost:8080"
-	ALBUMS_PATH = "/albums"
+	albums_PATH = "/albums"
 )
 
 //Respond with the list of all albums as JSON
 func getAlbums(context *gin.Context) {
-	context.IndentedJSON(http.StatusOK, ALBUMS)
+	context.IndentedJSON(http.StatusOK, albums)
+}
+
+//Add an album from JSON recieved in the request body
+func postAlbum(context *gin.Context) {
+	var newAlbum album
+	
+	//Call BindJSON to bind the recievedJSON to newAlbum
+	if err := context.BindJSON(&newAlbum); nil != err {
+		return
+	}
+
+	//Add the new album to the slice
+	albums = append(albums, newAlbum)
+	context.IntendedJSON(http.StatusCreated, newAlbum)
+
+
 }
 
 func main() {
 	router := gin.Default()
-	router.GET(ALBUMS_PATH, getAlbums)
+	router.GET(albums_PATH, getAlbums)
 	router.Run(PATH)
 }
