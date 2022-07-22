b28468b59119534879f937f0ef88aa5342d8124e
dangkl123 <nhatdang2604@gmail.com>
2022-07-22 22:50:27 +0700

    Add method to find album by id

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ var (
 
 	PATH = "localhost:8080"
 	ALBUMS_PATH = "/albums"
+	ALBUMS_ID_PARAM = "/:id"
 )
 
 //Respond with the list of all albums as JSON
@@ -46,9 +47,28 @@ func postAlbums(context *gin.Context) {
 
 }
 
+//Locate the album whose ID value matches the given id
+// parameter sent by the client, then returns that albums as response
+func getAlbumByID(context *gin.Context) {
+	ID_PARAM := "id"
+	id := context.Param(ID_PARAM)
+
+	//Find the album which has the id matching the given id
+	for _, alb := range albums {
+		if id == alb.ID {
+			context.IndentedJSON(http.StatusOK, alb)
+			return
+		}
+	}
+
+	context.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+
 func main() {
 	router := gin.Default()
 	router.GET(ALBUMS_PATH, getAlbums)
+	router.GET(ALBUMS_PATH + ALBUMS_ID_PARAM, getAlbumByID)
 	router.POST(ALBUMS_PATH, postAlbums)
 	router.Run(PATH)
 }
